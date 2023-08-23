47721f25c4264f6ad714851649fe6b1df9b259bc
zhoulin <zhoulin@xiangrong.pro>
2023-08-23 15:05:27 +0800

    安装和配置 tailwindcss 以及相关

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./src/**/*.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
